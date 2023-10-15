424048d343c5ba008f7fd0c14a800c97ad7ddcfe
MiguelGmz7 <oscar.gomez5178@alumnos.udg.mx>
2023-10-15 00:48:25 -0600

    Codigo funcionando

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import threading
 import time
 import queue
+import os
 
 # Codigo correcto
 
@@ -23,7 +24,7 @@ def cliente(cliente_id, cola_clientes, barbero_disponible):
     if cola_clientes.qsize() < 3:
         cola_clientes.put(cliente_id)
         if barbero_disponible.is_set():
-            print(f'El barbero está despierto. Cliente {cliente_id} despierta al barbero')
+            print(f'El barbero está despierto.')
             barbero_disponible.clear()
     else:
         print(f'La barbería está llena. Cliente {cliente_id} se va.')
@@ -32,16 +33,20 @@ if __name__ == '__main__':
     cola_clientes = queue.Queue() # Cola (FIFO)
     barbero_disponible = threading.Event() #Creamos un semaforo(False), Set = True, Clear = False, wait = empezera a ser true o false
 
+    clientes = []  # Número de clientes que llegan
+    total_c = input("Cuantos clientes deseas agregar? \n")
+    for i in range(int(total_c)):
+        clientes.append(i + 1)
+    os.system("cls")
 
     barbero_thread = threading.Thread(target=barbero,daemon=True, args=(barbero_disponible, cola_clientes)) #empieza la funcion como un hilo
     # Empeazamos un sub-proceso (hilo)
     barbero_thread.start()
 
-    clientes = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]  # Número de clientes que llegan
     for cliente_id in clientes:
         time.sleep(2)  # Intervalo de llegada de clientes
         cliente_thread = threading.Thread(target=cliente,daemon=True, args=(cliente_id, cola_clientes, barbero_disponible))
         #empezamos un sub-proceso
         cliente_thread.start()
     
-    input("Terminar el proceso")
+    input("")
